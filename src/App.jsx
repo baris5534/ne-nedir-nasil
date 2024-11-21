4629db9684d3e1935643dd7fcfe1937fcd1eabfe
baris5534 <samsuncrsmb55@gmail.com>
2024-11-21 18:47:00 +0300

    ilk hazırlık tamam

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 //import React from "react";
-import CodeDisplay from "./components/Codescreen";
-
+//import CodeDisplay from "./components/Codescreen";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
 const App = () => {
- 
-
   return (
     <div className="min-h-screen bg-gray-950 flex items-center justify-center p-4">
-      <CodeDisplay title="HelloWorld.js" code="merhaba" />
+      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import CodeScreen from "../components/Codescreen"
+
+export default function Home(){
+    return(
+        <div>
+            <CodeScreen title="App.js" code="import react from 'react'"/>
+        </div>
+    )
+}
