e60509531a63b6263d60f190b6195f5b6b38dff0
baris5534 <samsuncrsmb55@gmail.com>
2025-02-09 13:02:34 +0300

    jö

--- src/utils/categoryIcons.jsx
+++ src/utils/categoryIcons.jsx
@@ -1,72 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Icon from '../components/icons/index';
+import { CategoryIcon } from '../components/icons/CategoryIcons';
 
 // Kategori sıralaması
-export const categoryOrder = [
-    'önerilenler',
-    'yeni',
-    'html',
-    'css',
-    'javascript',
-    'typescript',
-    'python',
-    'react',
-    'nextjs',
-    'vitejs',
-    'vite',
-    'visualstudiocode',
-    'framermotion',
-    'tailwind',
-    'mobile',
-    'responsive',
-    'performance',
-    'seo'
+export const CATEGORY_ORDER = [
+  'JavaScript',
+  'React',
+  'Vue',
+  'Node.js',
+  'Python',
+  'Java',
+  'Vite.js',
+  'Next.js',
+  'Visual Studio Code'
 ];
 
-// Kategori ikonlarını Icon bileşeninden al
-export const CATEGORY_ICONS = {
-    react: <Icon name="react" />,
-    javascript: <Icon name="javascript" />,
-    python: <Icon name="python" />,
-    vue: <Icon name="vue" />,
-    angular: <Icon name="angular" />,
-    html: <Icon name="html" />,
-    css: <Icon name="css" />,
-    typescript: <Icon name="typescript" />,
-    nextjs: <Icon name="nextjs" />,
-    vitejs: <Icon name="vitejs" className="lg:hidden flex"/>,
-    vite: <Icon name="vite" className="lg:flex hidden" />,
-    reactrouter: <Icon name="react-router" />,
-    vscode: <Icon name="vscode" />,
-    seo: <Icon name="seo" />,
-    önerilenler: <Icon name="önerilenler" />,
-    yeni: <Icon name="yeni" />,
-    default: <Icon name="default" />,
-    github: <Icon name="github" />,
-    supabase: <Icon name="supabase" />,
-    firebase: <Icon name="firebase" />,
-    mongodb: <Icon name="mongodb" />,
-    mysql: <Icon name="mysql" />,
-    postgresql: <Icon name="postgresql" />,
-    redis: <Icon name="redis" />,
-    
-    
+// Kategori ikonu bileşeni
+export const Icon = ({ name, className }) => {
+  return <CategoryIcon name={name} className={className} />;
 };
 
-export const CategoryIcon = ({ name, size = 'sm' }) => {
-    const normalizedName = name?.toLowerCase().replace(/[\s.]/g, '') || '';
-    return <Icon name={normalizedName} size={size} />;
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string
 };
 
-CategoryIcon.propTypes = {
-    name: PropTypes.string,
-    size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl'])
-};
-
-CategoryIcon.defaultProps = {
-    name: '',
-    size: 'sm'
-};
-
-export default CategoryIcon; 
+export default Icon; 
